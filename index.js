c6a5a8cba79f0d6190831344bc54e6dac904ab0f
JonathanYin <joyin@ucsd.edu>
2022-12-09 11:30:06 -0800

    Fix jQuery issue

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-(function() {
-    $('.btn').click(function() {
-        $(this).toggleClass('active');
-        return $('.box').toggleClass('open');
-    });
-    
-}).call(this);
+// jQuery
+$(document).ready(function () {
+    // on click, toggle box elements
+    (function() {
+        $('.btn').click(function() {
+            $(this).toggleClass('active');
+            return $('.box').toggleClass('open');
+        });
+        
+    }).call(this);
+});

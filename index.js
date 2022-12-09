5d58855deab5ade71b75dde21f8abfdef652a6af
JonathanYin <joyin@ucsd.edu>
2022-12-09 11:02:52 -0800

    Cleanup

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// function openBox() {
-//     var box = document.getElementById("box");
-//     var btn = document.getElementById("btn");
-//     box.classList.toggle("open");
-//     btn.classList.toggle("active");
-//     // btn.click();
-// }
-
-function init() {
-    $(".btn").on('click', function() {
+(function() {
+    $('.btn').click(function() {
         $(this).toggleClass('active');
         return $('.box').toggleClass('open');
     });
-}
-
-init();
+    
+}).call(this);
